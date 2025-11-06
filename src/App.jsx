27e58d2fd59314e9540691223c1806b1d64e0791
pratiksha mengawade <pratikshamengawade666@gmail.com>
2025-11-06 11:53:49 +0530

    login change

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import EmployeeDashboard from "./components/EmployeeDashboard";
 import EmployeeProfile from "./components/EmployeeProfile";
-import Login from "./components/login";
 import Register from "./components/Register";
 import Welcome from "./components/Welcome";
+import LoginPage from "./components/LoginPage";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Welcome />}></Route>
 
-        <Route path="/login" element={<Login />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
 
         <Route path="/dashboard" element={<EmployeeDashboard />}></Route>
 
--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-function Login() {
+function LoginPage() {
   const [loginForm, setLoginForm] = useState({
     email: "",
     password: "",
@@ -100,4 +100,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default LoginPage;
