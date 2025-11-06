fd8e2dc113b4fbf31c46bd91871a187537d27d7f
pratiksha mengawade <pratikshamengawade666@gmail.com>
2025-11-06 11:30:30 +0530

    deploy on vercel

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Login() {
   const [loginForm, setLoginForm] = useState({
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import WelcomeMenu from './WelcomeMenu'
-import Footer from './footer'
+import Footer from './Footer'
 
 function Welcome() {
   return (
